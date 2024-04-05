7f99f27119f0818352868022e2c6a39fb052748d
Nayan Kumar <naayaankumar@gmail.com>
2024-04-05 11:25:04 +0530

    get and post operations done on clothing

---
+++ src/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const authRouter = require("./router/auth.router");
+const connect = require("./Config/db");
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json(), express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  try {
+    const message = "Backend ZigZig";
+    return res.status(200).send({ message });
+  } catch (er) {
+    return res.status(404).send({ message: er.message });
+  }
+});
+
+app.use("/auth", authRouter);
+
+app.listen(PORT, async () => {
+  await connect();
+  console.log(`backend running on http://localhost:${PORT}`);
+});
---
+++ src/model/Product.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const file = {
+  title: {
+    type: String,
+    maxlength: 20,
+    require: [true, "Please enter title"],
+    default: "Yet to add Title",
+  },
+  description: {
+    type: String,
+    require: [true, "Please Enter the description"],
+    default: "Yet to add description",
+  },
+  image: {
+    type: String,
+    require: [true, "Please add image"],
+    default:
+      "https://w1.pngwing.com/pngs/314/924/png-transparent-person-logo-symbol-man-black-black-and-white-silhouette.png",
+  },
+  price: { type: Number, require: [true, "Please enter price"] },
+  rate: {
+    type: Number,
+    require: [true, "Please enter the rating for each of clothes"],
+    default: 0,
+  },
+  offers: { type: Array, default: [] },
+  quantity: { type: Number, default: 0 },
+  gender: { enum: ["Male", "Female", "Non-binary"], default: "Male" },
+};
+
+const productSchema = new mongoose.Schema(file);
+const productModel = mongoose.model("clothing", productSchema);
+
+module.exports = productModel;
